f51f3fc59fb42a6fe1dce39c783d6ab34e9e58a8
Jesse Parent <jesseparent@gmail.com>
2020-10-09 18:47:31 -0600

    add new note with ID set to milliseconds since epoch

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,10 +1,22 @@
+const { findById, createNewNote, validateNote } = require('../../lib/notes');
 const { notes } = require('../../db/db');
 const router = require('express').Router();
 
 router.get('/notes', (req, res) => {
-  let results = notes;
-  
-  res.json(results);
+  res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+  // set id based on milliseconds since Epoch
+  req.body.id = new Date().getTime();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
 });
 
 module.exports = router;
